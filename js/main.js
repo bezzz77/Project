5a98be5bfaf4c90da108deec8a7cf9cb3518a1ba
bezzz77 <bezzz77@gmail.com>
2018-12-10 03:28:04 +0200

    edit commit

--- js/main.js
+++ js/main.js
@@ -23,5 +23,23 @@ $(document).ready(function(){
       'display' : 'none',
     });
   });
+  $(".slidercool__wrap").owlCarousel({
+    items:3,
+    autoPlay: 3000,
+    itemsDesktop: [1199, 3],
+    itemsMobile: [479, 1],
+  });
+
 
 });
+
+function showPopup()
+{
+  console.log($('#description__formtwo'));
+  $('.description__wrapblocktwo').show();
+  // $('.thirdsectionblock').css({
+  //   'display' : 'none',
+  // });
+  $('.description__wrap').hide();
+
+}
